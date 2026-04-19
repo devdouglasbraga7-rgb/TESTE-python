64fb50a072ac91cfc9e1f938881b1abdb825668e
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-04-19 14:10:46 -0300

    adicionando prints de multplicaçã

--- teste.py
+++ teste.py
@@ -5,4 +5,10 @@ for x in range(0, 100, 2):
 for x in range(1, 100, 2):
         multp *= x
 print(f"A soma dos 50 primgeiros números pares é = {soma}")
-print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
+print(f"A mutiplicação dos 50 primeiros números ímpares é = {multp}")  
